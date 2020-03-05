1747df8458e1bbe6c7a810e631b423c5e1b02c7a
SOOHYUNLIM <sh_7682@naver.com>
2020-03-05 17:23:43 +0900

    React Material 적용

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Paperbase from "./component/Paperbase";
+
+function App() {
+
+  return (
+    <Paperbase></Paperbase>
+  );
+}
+
+export default App;
